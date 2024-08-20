5eb303d9789ecb369a1f2d56205a87424634a610
aaron <china.jinshuo@gmail.com>
2024-08-20 15:18:24 +0800

    daily

--- src/layouts/components/Logo/index.vue
+++ src/layouts/components/Logo/index.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="flex items-center">
-    <img :src="logo" alt="logo" class="h-[22px] w-auto" />
+    <img :src="config.logo" alt="logo" class="h-[22px] w-auto" />
     <h1 v-if="showTitle" class="ml-2 text-lg text-black/88">{{ config.title }}</h1>
   </div>
 </template>
 
 <script setup lang="ts">
-import logo from '@/assets/logo.svg'
 import config from '@/config'
 
 interface Props {
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,43 +1,101 @@
 <template>
-  <div>
-    <el-form :model="form" label-width="auto" style="max-width: 600px">
-      <el-form-item label="用户名">
-        <el-input v-model="form.username" />
-      </el-form-item>
-      <el-form-item label="密码">
-        <el-input v-model="form.password" />
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="handleSubmit"> 确定 </el-button>
-      </el-form-item>
-    </el-form>
+  <div class="h-full flex flex-col">
+    <div class="grow px-6 py-14">
+      <div class="mb-10 flex items-center justify-center">
+        <img :src="config.logo" alt="logo" class="h-11 w-auto" />
+        <div class="ml-4 text-4xl font-bold">{{ config.title }}</div>
+      </div>
+      <div class="mx-auto w-80">
+        <el-form
+          ref="formRef"
+          :model="formData"
+          :rules="rules"
+          size="large"
+          @keyup.enter="handleSubmit"
+        >
+          <el-form-item prop="username">
+            <el-input
+              v-model="formData.username"
+              placeholder="用户名"
+              prefix-icon="User"
+              autocomplete="off"
+            />
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input
+              v-model="formData.password"
+              placeholder="密码"
+              prefix-icon="Lock"
+              show-password
+              autocomplete="off"
+            />
+          </el-form-item>
+          <el-form-item>
+            <el-button
+              class="w-full"
+              size="large"
+              type="primary"
+              :loading="loading"
+              @click="handleSubmit"
+              >确定</el-button
+            >
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+    <Footer />
   </div>
 </template>
 
 <script setup lang="ts">
+import config from '@/config'
 import { useUserStore } from '@/store/user'
 import { ApiError } from '@/utils/request'
+import Footer from '@/layouts/components/Footer/index.vue'
+import { FormInstance, FormRules } from 'element-plus'
 
 const userStore = useUserStore()
 const router = useRouter()
 
-const form = reactive({
-  username: '15150020157',
-  password: '1'
+const formRef = ref<FormInstance>()
+const loading = ref(false)
+
+// 15150020157
+const formData = reactive({
+  username: '',
+  password: ''
 })
 
+const rules: FormRules<typeof formData> = {
+  username: {
+    required: true,
+    message: '请输入用户名'
+  },
+  password: {
+    required: true,
+    message: '请输入密码'
+  }
+}
+
 const handleSubmit = () => {
-  userStore
-    .login(form)
-    .then(() => {
-      router.push('/')
-    })
-    .catch(err => {
-      if (err instanceof ApiError) {
-        ElMessage.error(err.data.message)
-      }
-    })
+  if (!formRef.value) return
+
+  formRef.value.validate(valid => {
+    if (valid) {
+      loading.value = true
+      userStore
+        .login(formData)
+        .then(() => {
+          ElMessage.success('登录成功')
+          router.push('/')
+        })
+        .catch(err => {
+          ElMessage.error(err instanceof ApiError ? err.data.message : err.message)
+        })
+        .finally(() => {
+          loading.value = false
+        })
+    }
+  })
 }
 </script>
-
-<style lang="scss" scoped></style>
