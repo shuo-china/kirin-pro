f1922e5d0da347196f311e143d2395193877fd1d
aaron <china.jinshuo@gmail.com>
2024-08-28 15:59:32 +0800

    daily

--- src/pages/user/list/index.vue
+++ src/pages/user/list/index.vue
@@ -1,13 +1,7 @@
 <template>
   <ProTable :request="getUserListApi">
     <ProTableColumn prop="id" label="ID" />
-    <ProTableColumn
-      prop="depart.depart_name"
-      label="部门"
-      search
-      search-type="select"
-      :search-options="searchOptions"
-    />
+    <ProTableColumn prop="depart.depart_name" label="部门" />
     <ProTableColumn prop="name" label="姓名" search />
     <ProTableColumn prop="last_login_ip" label="末次登录IP" />
     <ProTableColumn prop="create_time" label="创建时间" search search-type="date-picker" />
@@ -16,11 +10,4 @@
 
 <script setup lang="ts">
 import { getUserListApi } from '@/api/user'
-
-const searchOptions = [
-  { label: 'Label1', value: 'Value1' },
-  { label: 'Label2', value: 'Value2' }
-]
 </script>
-
-<style lang="scss" scoped></style>
